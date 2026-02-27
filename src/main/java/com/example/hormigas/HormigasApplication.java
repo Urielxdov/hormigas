eccbb1f9f6807aca3f13812135643f6e7443250b
urielxdov <urieledgar878@gmail.com>
2026-02-26 20:25:36 -0600

    Initial commit

---
+++ src/main/java/com/example/hormigas/HormigasApplication.java
@@ -0,0 +1,13 @@
+package com.example.hormigas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HormigasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HormigasApplication.class, args);
+	}
+
+}
